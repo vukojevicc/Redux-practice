4345bba7898389048219f83571679f570c7b12dd
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-19 23:16:24 +0100

    more practicing of Redux

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,17 @@
 const redux = require('redux')
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    return {
-        counter: state.counter + 1
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
     }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state
 }
 
 const store = redux.createStore(counterReducer)
@@ -15,3 +23,4 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber)
 store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
